1bebd5a20738ad9308ab35fe95e055384389968a
gryder <gryder@google.com>
2009-01-27 17:37:03 +0000

    Adds a release script.

---
+++ release.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Creates release archives.
+#
+# Copies the source code to a separate directory,
+# then compresses the directory in tar.gz and zip.
+# Deletes any previous version of the directory and the archives beforehand.
+
+NAME='google-shared-contacts-client'
+FILES='COPYING outlook.csv README.txt shared_contacts.py'
+
+# Go to the script directory.
+cd "$(readlink -f "$(dirname "$0")")"
+
+# Copy the files to a separate directory.
+rm -rf "${NAME}"
+mkdir -p "${NAME}"
+cp ${FILES} "${NAME}"
+
+# Delete the old archives
+rm -f "${NAME}.tar.gz" "${NAME}.zip"
+
+# Compress the directory in tar.gz and zip.
+tar cvzf "${NAME}.tar.gz" --owner=nobody --group=nobody "${NAME}/"
+zip -9 -r "${NAME}.zip" "${NAME}/"
